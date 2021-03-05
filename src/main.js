2f80b485b4c031009db3a101a5bbbf7c8f1ce8c1
zhangjiajin1 <582135144@qq.com>
2021-03-05 19:17:30 +0800

    登陆功能

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/fonts/iconfont.css'
+import './assets/css/global.css'
 import './plugins/element.js'
 
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
