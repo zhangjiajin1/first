317770d59b6c157c79afeeed343d10aa312907d1
zhangjiajin1 <582135144@qq.com>
2021-03-06 19:00:16 +0800

    主页

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ import './plugins/element.js'
 
 import axios from 'axios'
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+axios.interceptors.request.use(config =>{
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
