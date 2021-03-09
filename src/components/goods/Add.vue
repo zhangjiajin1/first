2ccfbc059ec915e8c774028210b21300426f67fd
zhangjiajin1 <582135144@qq.com>
2021-03-09 18:21:48 +0800

    商品列表

---
+++ src/components/goods/Add.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>添加商品</el-breadcrumb-item>
+    </el-breadcrumb>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+    },
+    methods:{}
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
