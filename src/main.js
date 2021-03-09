5d33f3a8296b066b13ecea9a0289fec70c58eacd
zhangjiajin1 <582135144@qq.com>
2021-03-09 21:59:01 +0800

    添加商品功能

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import './assets/css/global.css'
 import './plugins/element.js'
 import TreeTable from 'vue-table-with-tree-grid'
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 import axios from 'axios'
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
@@ -18,7 +23,7 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table',TreeTable)
-
+Vue.use(VueQuillEditor)
 Vue.filter('dateFormat', function(originVal) {
   const dt = new Date(originVal)
   const y = dt.getFullYear()
